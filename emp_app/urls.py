e3329aaeed00fbbe86d47301756f42c64a542dde
mallick-portfolio <tamal1714019@gmail.com>
2023-05-31 00:25:53 +0600

    added delete employe functionlity

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, addemploye, deleteEmp
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('add-emp/', addemploye, name='add-emp'),
+    path('delete-emp/<int:id>/', deleteEmp, name='delete-emp')
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Employe, Role, Dept
 # Create your views here.
 
@@ -13,10 +13,28 @@ def home(request):
     return render(request, 'home.html', context)
 
 
-def get_all_employe(request):
-    emps = Employe.objects.all()
-    context = {
-        "emps": emps
-    }
+def addemploye(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = request.POST['phone']
+        dept = int(request.POST['dept'])
 
-    return render(request, 'emps.html', context)
+        role = int(request.POST['role'])
+        department = Dept.objects.get(id=dept)
+        emp_role = Role.objects.get(id=role)
+        new_emp = Employe(first_name=first_name, last_name=last_name,
+                          salary=salary, bonus=bonus, phone=phone, dept=department, role=emp_role, )
+        new_emp.save()
+        return HttpResponse("Employe added successfully")
+    else:
+        return render(request, 'add_emp.html')
+
+
+def deleteEmp(request, id):
+    emp = Employe.objects.filter(id=id).first()
+    emp.delete()
+    return redirect('/')
+    # return HttpResponse("Employee Removed Successfully")
